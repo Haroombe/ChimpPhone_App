c98590ee21d1b61a7ec10268341bb4708859bcce
Haroon Khan <MohammedHaroonKhan19@gmail.com>
2024-12-04 11:20:51 -0600

    added winston

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,18 @@
 // utils/db.js
 
 import { Pool } from 'pg';
+import winston from 'winston';
+
+// Configure Winston logger
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File({
+      filename: 'query.sql',
+      level: 'info',
+      format: winston.format.printf(({ message }) => message), // Log only the message
+    }),
+  ],
+});
 
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
@@ -13,5 +25,13 @@ const pool = new Pool({
 pool.on('connect', () => {
   console.log('Connected to the database');
 });
+
+// Override pool.query to log queries
+const originalQuery = pool.query.bind(pool);
+pool.query = (...args) => {
+  const queryText = args[0];
+  logger.info(queryText);
+  return originalQuery(...args);
+};
 
 export { pool };
